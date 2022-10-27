f0cea5314f00f128dbe16e3d049a3ef103dd360e
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-10-27 15:31:53 +0200

    snack 1

---
+++ main.js
@@ -0,0 +1,33 @@
+let parola = prompt("inserisci una parola");
+
+console.log(`Questa è la parola inserita: ${parola}`);
+
+
+function pailndromo() {
+    let parolaInversa = "";
+
+    for (let i = parola.length - 1; i >= 0; i--) {
+        parolaInversa += parola[i]; 
+    }
+    
+    console.log(parolaInversa);
+
+    if(parolaInversa == parola){
+        console.log("La tua parola è Palindroma");
+    }
+    else{
+        console.log("Non è Palindroma")
+    }
+}
+
+
+pailndromo();
+
+
+
+
+
+
+
+
+
